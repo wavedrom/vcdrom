1b62980288678730d7e2983b55827d11016c02e0
Kevin Mills <kevin.mills@sifive.com>
2022-06-24 10:36:55 -0700

    cp favison folder to app

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ cp node_modules/codemirror/lib/codemirror.css app
 cp node_modules/vcd-stream/out/vcd.wasm app
 
 cp src/vcdrom.html app/index.html
+cp -r src/favicon app
 cp src/vcdrom.css app
 cp src/*.woff2 app
 
