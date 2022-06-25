ad7c86728a3c2b225404a293d461779397394461
Anirudh Suresh Ramchandran <aniramch@ucdavis.edu>
2022-06-24 19:03:59 -0700

    [nav help menu] [- this commit adds a help nav bar to vcdrom - uses css opacity toggled from 0-1 to display the help menu ]

--- build.sh
+++ build.sh
@@ -9,5 +9,7 @@ cp node_modules/vcd-stream/out/vcd.wasm app
 cp src/vcdrom.html app/index.html
 cp src/vcdrom.css app
 cp src/*.woff2 app
+cp src/Freedom.svg app/Freedom.svg
+
 
 ./node_modules/.bin/browserify ./lib/vcdrom.js | ./node_modules/.bin/terser --compress -o app/vcdrom.js
