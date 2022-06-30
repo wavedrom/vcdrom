fe310af4ff501fb989a18205e592a2cd51c45f18
Kevin Mills <kevin.mills@sifive.com>
2022-06-30 09:53:46 -0700

    add abs and relative cusor sync

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "prettier/prettier": "error",
+  },
+  plugins: ["prettier"],
+};
