c6c7cec8a44775cd25c0052653482b2c4435c53c
Kevin Mills <kevin.mills@sifive.com>
2022-10-31 12:40:24 -0700

    use local doopler dep, and build script

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/bash
+debug=false
+
+while getopts dj flag
+do
+    case "${flag}" in
+        d) 
+            debug=true ;;
+    esac
+done
+
+if [ ! -d "node_modules" ]; then
+    npm i
+fi
 
 mkdir -p app
 
@@ -10,4 +23,13 @@ cp src/vcdrom.html app/index.html
 cp src/vcdrom.css app
 cp src/*.woff2 app
 
-./node_modules/.bin/browserify ./lib/vcdrom.js | ./node_modules/.bin/terser --compress -o app/vcdrom.js
+
+if [ "${debug}" == "true" ]
+then
+    # When debugging don't tersify and do generate source maps.
+    ./node_modules/.bin/browserify -d ./lib/vcdrom.js >app/vcdrom.js
+else
+    ./node_modules/.bin/browserify ./lib/vcdrom.js | ./node_modules/.bin/terser --compress -o app/vcdrom.js
+fi
+
+
