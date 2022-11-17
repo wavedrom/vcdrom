9516e2458c85e0f111f28102377396688c052f9f
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2022-11-16 21:39:32 -0800

    CSS in JS

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ mkdir -p app
 cp node_modules/vcd-stream/out/vcd.wasm app
 
 cp src/vcdrom.html app/index.html
-cp src/vcdrom.css app
 cp src/*.woff2 app
 
 ./node_modules/.bin/browserify ./lib/vcdrom.js | ./node_modules/.bin/terser --compress -o app/vcdrom.js
